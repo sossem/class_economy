81c20cd751903b7b54159aeff9224fdbbeede4eb
sossem <29ssone@naver.com>
2026-04-14 23:16:06 +0900

    base 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/https://github.com/sossem/class_economy.git/', /* <--- 여기에 선생님의 Github 저장소 이름을 적어주세요! */
+  base: '/class_economy/', /* <--- 여기에 선생님의 Github 저장소 이름을 적어주세요! */
 })
